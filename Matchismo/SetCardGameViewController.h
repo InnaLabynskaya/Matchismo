8bd60d8244951c877a24ee68c3c8b8114d9509bc
Inna Labuskaya <innalabynskaya@gmail.com>
2015-01-22 00:30:42 +0200

    Added Bar Control

---
+++ Matchismo/SetCardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetCardGameViewController.h
+//  Matchismo
+//
+//  Created by Inna Labuskaya on 1/21/15.
+//  Copyright (c) 2015 Inna Labuskaya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SetCardGameViewController : ViewController
+
+@end
---
+++ Matchismo/SetCardGameViewController.m
@@ -0,0 +1,37 @@
+//
+//  SetCardGameViewController.m
+//  Matchismo
+//
+//  Created by Inna Labuskaya on 1/21/15.
+//  Copyright (c) 2015 Inna Labuskaya. All rights reserved.
+//
+
+#import "SetCardGameViewController.h"
+
+@interface SetCardGameViewController ()
+
+@end
+
+@implementation SetCardGameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
