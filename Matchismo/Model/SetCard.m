5abe137b05775f05175fc9355b9208f1567fd55f
Inna Labuskaya <innalabynskaya@gmail.com>
2015-01-23 21:55:14 +0200

    Match function has been implemented

--- Matchismo/Model/SetCard.m
+++ Matchismo/Model/SetCard.m
@@ -10,9 +10,36 @@
 
 @implementation SetCard
 
+static const NSUInteger sMaxNumber = 3;
+
 + (NSUInteger)maxNumber
 {
-    return 3;
+    return sMaxNumber;
+}
+
+- (int) match:(NSArray *) otherCards
+{
+    int score = 0;
+    if([otherCards count] == sMaxNumber - 1) {
+        NSUInteger sumNumber = self.number;
+        NSUInteger sumColor = self.color;
+        NSUInteger sumShading = self.shading;
+        NSUInteger sumShape = self.shape;
+        for(SetCard *card in otherCards) {
+            sumNumber += card.number;
+            sumColor += card.color;
+            sumShading += card.shading;
+            sumShape += card.shape;
+        }
+        if(sumNumber % sMaxNumber == 0 &&
+           sumColor % sMaxNumber == 0 &&
+           sumShading % sMaxNumber == 0 &&
+           sumShape % sMaxNumber == 0)
+        {
+            score = 1;
+        }
+    }
+    return score;
 }
 
 @end
